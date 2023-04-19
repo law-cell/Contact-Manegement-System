7181d6956598bd0dae11cf5128681edd4b0e9d99
junhao <junhaoluo0705@gmail.com>
2023-04-19 14:44:00 +0100

    import Vue;

---
+++ vue_test/src/components/Contact.vue
@@ -0,0 +1,28 @@
+<template>
+    <p>{{ message }} {{ name }}</p>
+    <button v-on:click="getUser()">Get Random User</button>
+</template>
+
+<script>
+
+export default {
+    name: "ContactItem",
+    methods: {
+        async getUser() {
+            const res = await fetch('https://randomuser.me/api')
+            const {results} = await res.json()
+            console.log(results)
+        },
+
+        async testAPI() {
+            const res = await fetch('http://localhost:8080/api/v1/people/getAllPeople')
+            const results = await res.json()
+            console.log(results[0].name)
+        },
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
